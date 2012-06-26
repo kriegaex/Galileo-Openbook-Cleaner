fd093cf5e08f5c584da398901fcf86477bbc80be
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-06-26 13:46:51 +0200

    Add utility classes FileDownloader (incl. MD5 check), ZipFileExtractor
    This is a preparation for adding download, check MD5, unpack features
    to OpenbookCleaner later.

---
+++ galileo_openbook_cleaner/src/de/scrum_master/util/FileDownloader.java
@@ -0,0 +1,114 @@
+package de.scrum_master.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FileDownloader
+{
+	private URL        from;
+	private File       to;
+	private BigInteger md5;
+	private boolean    doChecksum;
+
+	public class MD5MismatchException extends Exception
+	{
+		private File file;
+		private BigInteger md5Expected;
+		private BigInteger md5Actual;
+
+		private static final long serialVersionUID = 5271376926114647849L;
+
+		MD5MismatchException(File file, BigInteger md5Expected, BigInteger md5Actual) {
+			super(file + ": expected " + md5Expected.toString(16) + ", got " + md5Actual.toString(16));
+			this.file = file;
+			this.md5Expected = md5Expected;
+			this.md5Actual = md5Actual;
+		}
+
+		public File getFile() { return file; }
+		public BigInteger getMD5Expected() { return md5Expected; }
+		public BigInteger getMD5Actual() { return md5Actual; }
+	}
+
+	public FileDownloader(URL from, File to, BigInteger md5)
+	{
+		if (from == null)
+			throw new IllegalArgumentException("Parameter 'from' must not be null");
+		if (to == null)
+			throw new IllegalArgumentException("Parameter 'to' must not be null");
+		this.from = from;
+		this.to = to;
+		this.md5 = md5;
+		this.doChecksum = md5 != null;
+	}
+
+	public FileDownloader(String fromURL, String toFile, String md5) throws MalformedURLException
+	{
+		this(
+			new URL(fromURL),
+			new File(toFile),
+			md5 == null ? null : new BigInteger(md5, 16)
+		);
+	}
+
+	public FileDownloader(URL from, File to)
+	{
+		this(from, to, null);
+	}
+
+	public FileDownloader(String fromURL, String toFile) throws MalformedURLException
+	{
+		this(fromURL, toFile, null);
+	}
+
+	public void download()
+		throws IOException, NoSuchAlgorithmException, MD5MismatchException
+	{
+		OutputStream        outStream;
+		MessageDigest       md5Digest  = null;
+		ReadableByteChannel in         = null;
+		WritableByteChannel out        = null;
+		ByteBuffer          buffer;
+		BigInteger          md5Actual; 
+		try {
+			outStream = new FileOutputStream(to);
+			if (doChecksum) {
+				md5Digest = MessageDigest.getInstance("MD5");
+				outStream = new DigestOutputStream(outStream, md5Digest);
+			}
+			in = Channels.newChannel(from.openStream());
+			out = Channels.newChannel(outStream);
+			buffer = ByteBuffer.allocate(1 << 20);  // 1 MB
+		
+			// Download file, optionally calculate MD5 while writing output
+			while (in.read(buffer) != -1) {
+				buffer.flip();
+				out.write(buffer);
+				buffer.clear();
+			}
+
+			if (doChecksum) {
+				md5Actual = new BigInteger(1, md5Digest.digest()); 
+				if (! md5Actual.equals(md5))
+					throw new MD5MismatchException(to, md5, md5Actual);
+			}
+		}
+		finally {
+			try { in.close(); }  catch (IOException e) { }
+			try { out.close(); } catch (IOException e) { }
+		}
+	}
+
+}
---
+++ galileo_openbook_cleaner/src/de/scrum_master/util/ZipFileExtractor.java
@@ -0,0 +1,67 @@
+package de.scrum_master.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ZipFileExtractor
+{
+	private File archiveFile;
+	private File targetDirectory;
+
+	private static final int BUFFER_SIZE = 128 * 1024;
+
+	public ZipFileExtractor(File archiveFile, File targetDirectory)
+	{
+		if (archiveFile == null)
+			throw new IllegalArgumentException("Parameter 'archiveFile' must not be null");
+		if (targetDirectory == null)
+			throw new IllegalArgumentException("Parameter 'targetDirectory' must not be null");
+		this.archiveFile = archiveFile;
+		this.targetDirectory = targetDirectory;
+	}
+
+	public ZipFileExtractor(String archiveFile, String targetDirectory)
+	{
+		this(new File(archiveFile), new File(targetDirectory));
+	}
+
+	public void unzip() throws IOException
+	{
+		SimpleLogger.debug("Unzipping archive " + archiveFile + " ...");
+
+		ZipInputStream       zipStream     = null;
+		ZipEntry             zipEntry;
+		byte[]               buffer        = new byte[BUFFER_SIZE];
+		int                  byteCount;
+		File                 unzippedFile;
+		BufferedOutputStream outUnzipped   = null;
+
+		try {
+			zipStream = new ZipInputStream(new BufferedInputStream(new FileInputStream(archiveFile), BUFFER_SIZE));
+			while ((zipEntry = zipStream.getNextEntry()) != null) {
+				SimpleLogger.debug("  Extracting " + zipEntry);
+				unzippedFile = new File(targetDirectory, zipEntry.getName());
+				if (zipEntry.isDirectory()) {
+					unzippedFile.mkdirs();
+					continue;
+				}
+				unzippedFile.getParentFile().mkdirs();
+				outUnzipped = new BufferedOutputStream(new FileOutputStream(unzippedFile), BUFFER_SIZE);
+				while ((byteCount = zipStream.read(buffer, 0, BUFFER_SIZE)) != -1)
+					outUnzipped.write(buffer, 0, byteCount);
+				outUnzipped.close();
+			}
+		}
+		finally {
+			try { outUnzipped.close(); } catch (IOException e) { }
+			try { zipStream.close(); }   catch (IOException e) { }
+		}
+		SimpleLogger.debug("Unzipping done");
+	}
+}
