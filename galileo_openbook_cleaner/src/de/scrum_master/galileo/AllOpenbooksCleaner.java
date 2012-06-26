93e573082658881990645cbcabde6dd53101925f
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-06-27 00:59:29 +0200

    Adjust class AllOpenbooksCleaner to CLI changes in OpenbookCleaner
    Because the CLI (command line interface) of OpenbookCleaner has changed
    previously, AllOpenbooksCleaner must call its main method differently
    now.

--- galileo_openbook_cleaner/src/de/scrum_master/galileo/AllOpenbooksCleaner.java
+++ galileo_openbook_cleaner/src/de/scrum_master/galileo/AllOpenbooksCleaner.java
@@ -6,16 +6,17 @@ import de.scrum_master.util.SimpleLogger;
 
 public class AllOpenbooksCleaner
 {
-	private static final String basePath = "c:/Dokumente und Einstellungen/Robin/Eigene Dateien/Bücher/Galileo Computing/";
+	private static final String downloadDir = "c:/Dokumente und Einstellungen/Robin/Eigene Dateien/Bücher/Galileo Computing/";
 
 	public static void main(String[] args) throws Exception
 	{
 		long startTime = System.currentTimeMillis();
 		// Forward command line options to OpenbookCleaner.main, always adding current book's name at the end
 		int argv = args.length;
-		String[] newArgs = Arrays.copyOf(args, argv + 1);
+		String[] newArgs = Arrays.copyOf(args, argv + 2);
 		for (BookInfo bookInfo : BookInfo.values()) {
-			newArgs[argv] = basePath + bookInfo.unpackDirectory;
+			newArgs[argv] = downloadDir;
+			newArgs[argv + 1] = bookInfo.unpackDirectory;
 			OpenbookCleaner.main(newArgs);
 			SimpleLogger.echo("");
 		}
