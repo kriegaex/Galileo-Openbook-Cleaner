20ae86df0c2448d0ecd454fe746fa907672c0bfc
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-06-27 01:13:32 +0200

    AllOpenbooksCleaner: change hard-coded downloadDir into CLI parameter
    Now AllOpenbooksCleaner also needs the downloadDir as a command line
    parameter, just like OpenbookCleaner. The parameter is forwared to
    OpenbookCleaner alright, just like -s, -d etc.

--- galileo_openbook_cleaner/src/de/scrum_master/galileo/AllOpenbooksCleaner.java
+++ galileo_openbook_cleaner/src/de/scrum_master/galileo/AllOpenbooksCleaner.java
@@ -6,17 +6,14 @@ import de.scrum_master.util.SimpleLogger;
 
 public class AllOpenbooksCleaner
 {
-	private static final String downloadDir = "c:/Dokumente und Einstellungen/Robin/Eigene Dateien/Bücher/Galileo Computing/";
-
 	public static void main(String[] args) throws Exception
 	{
 		long startTime = System.currentTimeMillis();
 		// Forward command line options to OpenbookCleaner.main, always adding current book's name at the end
 		int argv = args.length;
-		String[] newArgs = Arrays.copyOf(args, argv + 2);
+		String[] newArgs = Arrays.copyOf(args, argv + 1);
 		for (BookInfo bookInfo : BookInfo.values()) {
-			newArgs[argv] = downloadDir;
-			newArgs[argv + 1] = bookInfo.unpackDirectory;
+			newArgs[argv] = bookInfo.unpackDirectory;
 			OpenbookCleaner.main(newArgs);
 			SimpleLogger.echo("");
 		}
