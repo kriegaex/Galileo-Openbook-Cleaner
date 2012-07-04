0eb01e87c21757202be64e25cb57497038ddcaa2
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-07-04 16:45:02 +0200

    SimpleLogger: optionally log thread ID (static flag LOG_THREAD_ID)
    If the flag is true, the log is written in tab-separated format (two
    columns: thread ID and indentation + log message) which can be imported
    into Excel easily via copy & paste. Then just add a header row, convert
    into table layout and there you go: filtering, sorting etc. are at your
    command, making it easy to see what happens in multi-threaded mode.
    
    TODO: time stamps might be useful - or overkill. No need for them just
    now.

--- galileo_openbook_cleaner/src/de/scrum_master/util/SimpleLogger.java
+++ galileo_openbook_cleaner/src/de/scrum_master/util/SimpleLogger.java
@@ -13,6 +13,8 @@ public class SimpleLogger
 	public static boolean ERROR   = true;
 	public static boolean TIME    = true;
 
+	public static boolean LOG_THREAD_ID = false;
+
 	private static InheritableThreadLocal<Integer> indentLevel =
 		new InheritableThreadLocal<Integer>() {
 			@Override protected Integer initialValue() { return 0; }
@@ -47,6 +49,15 @@ public class SimpleLogger
 	private static void log(PrintStream channel, boolean isActive, String message) {
 		if (!isActive)
 			return;
-		channel.println(indentText.get() + message);
+		if (LOG_THREAD_ID) {
+			/* Tab-separated format can be imported into Excel easily via copy & paste.
+			 * Add a header row, convert into table layout and there you go: filtering, sorting etc.
+			 * are at your command, making it easy to see what happens in multi-threaded mode mode.
+			 * TODO: time stamps might be useful - or overkill. No need for then just now.
+			 */
+			channel.printf("%5d\t%s\n", Thread.currentThread().getId(), indentText.get() + message);
+		}
+		else
+			channel.println(indentText.get() + message);
 	}
 }
