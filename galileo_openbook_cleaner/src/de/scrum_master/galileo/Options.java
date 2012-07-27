cb9f56512bd102cfa95458b8e568478892a1ddea
Alexander Kriegisch <Alexander@Kriegisch.name>
2012-07-27 12:34:02 +0200

    Options.printHelpOn: add usage line to help output

--- galileo_openbook_cleaner/src/de/scrum_master/galileo/Options.java
+++ galileo_openbook_cleaner/src/de/scrum_master/galileo/Options.java
@@ -73,6 +73,7 @@ class Options extends OptionParser {
 	}
 
 	public void printHelpOn(PrintStream sink, String errorMessage) throws IOException {
+		sink.println(OpenbookCleaner.class.getSimpleName() + " usage: java ... [options] <book_id>*\n");
 		printHelpOn(sink);
 		sink.println("book_id1 book_id2 ...                   Books to be downloaded & converted");
 		sink.println("\nLegal book IDs:");
