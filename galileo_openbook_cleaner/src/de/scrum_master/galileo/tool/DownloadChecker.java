b40730863627582db91e472cb16ce4de63a0a403
Alexander Kriegisch <Alexander@Kriegisch.name>
2013-03-10 09:19:03 +0100

    New tool class DownloadChecker checks downloads and verifies their MD5
    The class has a 'main' method which can be called by advanced users who
    know it.

---
+++ galileo_openbook_cleaner/src/de/scrum_master/galileo/tool/DownloadChecker.java
@@ -0,0 +1,32 @@
+package de.scrum_master.galileo.tool;
+
+import java.math.BigInteger;
+import java.net.URL;
+
+import de.scrum_master.galileo.Book;
+import de.scrum_master.util.FileDownloader;
+
+public class DownloadChecker {
+	/**
+	 * Checks <b>all</b> Galileo Openbooks for availability by downloading them and verifying their MD5 checksums.
+	 *
+	 * <b>Attention:</b> This is a long-running operation (~14 minutes on my old PC using 16 Mbit/s downstream DSL)
+	 * which as of 2013-03-10 downloads at least 1.1 Gb of data!
+	 */
+	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
+		Book.readConfig(true);
+		for (String bookID : Book.books.keySet()) {
+			Book book = Book.books.get(bookID);
+			System.out.print(bookID + " ... ");
+			try {
+				new FileDownloader(new URL(book.downloadArchive), null, new BigInteger(book.archiveMD5, 16)).download();
+				System.out.println("OK");
+			} catch (Exception e) {
+				System.out.println("failed");
+				e.printStackTrace();
+			}
+		}
+		System.out.println("\nExecution time: " + (System.currentTimeMillis() - startTime) / 1000.0 + " s");
+	}
+}
